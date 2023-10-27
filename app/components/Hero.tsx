6d4f871c664e0e79df5112bbeb76629935c26233
Zulekha Bibi <106396856+Zulelee@users.noreply.github.com>
2023-10-27 08:52:55 +0000

    Update 7 files

---
+++ app/components/Hero.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const hero = () => {
+  return (
+    <>
+      <div className="bg-black flex w-auto h-auto">
+        <div className="bg-white">
+          <h1>iLearn</h1>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default hero;
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 
 const page = () => {
   return (
     <main>
       <Navbar></Navbar>
+      <Hero></Hero>
     </main>
   );
 };
