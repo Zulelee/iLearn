d28924e5f47b25db897b802e91bf00a1cca5f664
Zulekha <zulekhaimtiaz1014@gmail.com>
2023-11-21 19:08:28 +0500

    animation added to features page

--- components/FeatureBox.tsx
+++ components/FeatureBox.tsx
@@ -9,8 +9,14 @@ interface BoxDetails {
 const FeatureBox = (details: BoxDetails) => {
   return (
     <>
-      <div className="flex flex-row">
-        <div className="flex flex-col max-w-sm m-5 bg-grey p-5 rounded-lg">
+      <div
+        className="flex flex-row m-5 bg-grey p-5 rounded-lg max-w-sm"
+        style={{
+          backgroundColor: "#2b2d33",
+          backgroundImage: 'url("architect.svg")',
+        }}
+      >
+        <div className="flex flex-col ">
           <div className="flex items-center justify-center">
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -29,7 +35,7 @@ const FeatureBox = (details: BoxDetails) => {
             <h1 className="text-lg">{details.title}</h1>
           </div>
 
-          <p className="font-extralight text-sm text-justify">
+          <p className="font-extralight text-sm text-left">
             {details.paragraph}
           </p>
         </div>
